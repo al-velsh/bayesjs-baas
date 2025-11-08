19205bea01008aa4abc31def5d807ca777958923
aaroncr <aaron.caballero@jetbrains.com>
2025-11-08 16:54:29 +0100

    chore(build): upload new build of a project
    Refs: Grafana-app-baas-5

--- dist/inferences/junctionTree/get-cliques-potentials.d.ts
+++ dist/inferences/junctionTree/get-cliques-potentials.d.ts
@@ -1,3 +1,3 @@
-import { IClique, ICliquePotentials, IGraph, INetwork, ISepSet, IEvidence } from '../../types';
-declare const _default: (cliques: IClique[], network: INetwork, junctionTree: IGraph, sepSets: ISepSet[], given: IEvidence, softEvidence?: Record<string, Record<string, number>> | undefined) => ICliquePotentials;
+import { IClique, ICliquePotentials, IGraph, INetwork, ISepSet } from '../../types';
+declare const _default: (cliques: IClique[], network: INetwork, junctionTree: IGraph, sepSets: ISepSet[], given: Record<string, string | Record<string, number>>, softEvidence?: Record<string, Record<string, number>> | undefined) => ICliquePotentials;
 export default _default;
--- dist/inferences/junctionTree/index.d.ts
+++ dist/inferences/junctionTree/index.d.ts
@@ -1,2 +1,4 @@
-import { IInfer } from '../../types';
+import { IInfer, INetwork, IRawInfer } from '../../types';
 export declare const infer: IInfer;
+export declare const rawInfer: (network: INetwork, given?: Record<string, string | Record<string, number>>) => IRawInfer;
+export declare const getPreNormalizedPotentials: (network: INetwork, given?: Record<string, string | Record<string, number>>) => IRawInfer;
