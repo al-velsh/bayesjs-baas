76559f55d99b2b53ee0b7d2a3cd52e96566de0d1
aaroncr <aaron.caballero@jetbrains.com>
2025-11-11 18:02:22 +0100

    feat(testing): Uploading fail unit test for learning in one variable

--- test/utils/parameter-learning.test.ts
+++ test/utils/parameter-learning.test.ts
@@ -2,15 +2,26 @@ import * as expect from 'expect'
 
 import { createNetwork } from '../../src/utils'
 import { allNodes, completeDataSetParentChild } from '../../models/parent-child-zero-knowledge-cpt'
-import { ICptWithParents, INetwork } from '../../src'
+import {ICptWithoutParents, ICptWithParents, INetwork} from '../../src'
 import { learningFromEvidence } from '../../src/utils/parameter-learning'
 import { allNodesSprinkler, missingWhetherRainDataSetSprinkler } from '../../models/extended-sprinkler'
+import {node1, simplestEvindence} from "../../models/simplest-model";
 
 const microNetwork = createNetwork(...allNodes)
 const sprinklerExtendedNetwork = createNetwork(...allNodesSprinkler)
+const simplestNetwork = createNetwork(node1)
 
 describe('Learning Utils', () => {
   describe('Simple learning examples', () => {
+
+    it('Learning with all evidence been the same should result in a CPT with the same values as evidence', () => {
+      let preLearningNetwork = JSON.parse(JSON.stringify(simplestNetwork))
+      const newNetwork = learningFromEvidence(preLearningNetwork, simplestEvindence)
+      const newCpt = newNetwork["Node_1"].cpt as ICptWithoutParents
+      expect(newCpt.True).toBeCloseTo(0.12, 1)
+      expect(newCpt.False).toBeCloseTo(0.88, 1)
+    })
+
     it('Learn form a complete dataset', () => {
       let newNetwork: INetwork = JSON.parse(JSON.stringify(microNetwork))
 
