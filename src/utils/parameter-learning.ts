de9785e4ad74a73114f905b12f1c67e542e0df70
aaroncr <aaron.caballero@jetbrains.com>
2025-11-28 16:36:13 +0100

    fix(learning): add/improve comments and minor refactoring
    Refs: Grafana-app-baas-3

--- src/utils/parameter-learning.ts
+++ src/utils/parameter-learning.ts
@@ -41,7 +41,16 @@ function getPotentialForFamily (potential: ICliquePotentialItem[], family: strin
   return newPotentials
 }
 
-function maximizationStep (network: INetwork, originalExpectedCounts: Record<string, ICliquePotentialItem[]>) {
+/**
+ *
+ * Performs the maximization step of the EM algorithm, that is equivalent to the maximization likelihood algorithm.
+ * This step is used to update the parameters of the nodes in the network, using the counts provided by the expectation step.
+ *
+ * @param network - The Bayesian network to update
+ * @param originalExpectedCounts - A map of counts of how much times a given configuration has been seen, for each node in the network.
+ * @returns A new network with the updated parameters, after the maximization step.
+ */
+function maximizationStep (network: INetwork, originalExpectedCounts: Record<string, ICliquePotentialItem[]>): INetwork {
   const newNetwork: INetwork = JSON.parse(JSON.stringify(network))
   const expectedCounts: Record<string, ICliquePotentialItem[]> = JSON.parse(JSON.stringify(originalExpectedCounts))
 
@@ -100,6 +109,15 @@ function maximizationStep (network: INetwork, originalExpectedCounts: Record<str
   return newNetwork
 }
 
+/**
+ *
+ * Returns the log likelihood of the network, compare with the evidence scenarios already process as counts.
+ * This metric is used to evaluate how close is the current network to the evidence scenarios, which is usually a negative number,.
+ *
+ * @param network - The Bayesian network to analyze the log likelihood of
+ * @param originalExpectedCounts - A map of counts of how much times a given configuration has been seen, for each node in the network.
+ * @returns A number representing the log likelihood of the network, compare with the evidence scenarios already process as counts.
+ */
 function computeCompleteDataLogLikelihood (network: INetwork, originalExpectedCounts: Record<string, ICliquePotentialItem[]>) {
   let logLikelihood = 0
 
@@ -108,21 +126,21 @@ function computeCompleteDataLogLikelihood (network: INetwork, originalExpectedCo
     const potentials = originalExpectedCounts[nodeId]
     for (const potential of potentials) {
       const countContext = potential.when
-      let cptProbabiltyOfContext = 0
+      let cptProbabilityOfContext = 0
       if (node.parents.length === 0) {
         const cpt = node.cpt as ICptWithoutParents
-        cptProbabiltyOfContext = cpt[countContext[nodeId]]
+        cptProbabilityOfContext = cpt[countContext[nodeId]]
       } else {
         const cpt = node.cpt as ICptWithParents
         const entryOfContext = cpt.find(x => Object.keys(x.when).every(key => countContext[key] === x.when[key]))
         if (entryOfContext) {
-          cptProbabiltyOfContext = entryOfContext.then[countContext[nodeId]]
+          cptProbabilityOfContext = entryOfContext.then[countContext[nodeId]]
         } else {
           throw Error('Implementation error: no entry of context was found')
         }
       }
 
-      logLikelihood += potential.then * Math.log(cptProbabiltyOfContext)
+      logLikelihood += potential.then * Math.log(cptProbabilityOfContext)
     }
   }
 
@@ -130,11 +148,13 @@ function computeCompleteDataLogLikelihood (network: INetwork, originalExpectedCo
 }
 
 /**
- * Performs iterative learning from evidence using the EM algorithm.
+ * Performs the expectation step of the EM algorithm.
+ * This step is used to compute the marginal probabilities of the nodes in the network,
+ * for each given evidence, given as a result a count of how much time a configuration has been seen.
  *
- * @param network - The initial Bayesian network to be trained
+ * @param network - The Bayesian network to be performance the expectation
  * @param given - Array of evidence instances for training
- * @returns Updated network after training with the specified parameters
+ * @returns A map of counts of how much time a given configuration has been seen, for each node in the network.
  */
 function expectationStep (network: INetwork, given: IEvidence[] = []): Record<string, ICliquePotentialItem[]> {
   const newGiven = JSON.parse(JSON.stringify(given))
@@ -165,6 +185,14 @@ function expectationStep (network: INetwork, given: IEvidence[] = []): Record<st
   return expectedCounts
 }
 
+/**
+ * Performs iterative learning from evidence using the EM algorithm.
+ *
+ * @param network - The initial Bayesian network to be trained
+ * @param given - Array of evidence instances for training
+ * @param stopRatio - The ratio of difference between the log likelihood of the current network and the previous one to stop the training
+ * @returns Updated network after training with the specified parameters
+ */
 export function learningFromEvidence (network: INetwork, given: IEvidence[] = [], stopRatio = 0.00001): INetwork {
   const MAX_ITERATIONS_SAFETY_LIMIT = 100
 
