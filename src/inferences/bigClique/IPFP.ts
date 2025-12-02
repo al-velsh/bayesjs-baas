cf9d8210079b2598500e215850fc6fca24f06e50
aaroncr <aaron.caballero@jetbrains.com>
2025-12-02 18:30:11 +0100

    feat(ipfp): improve comments IPFP
    Refs: Grafana-app-baas-19

--- src/inferences/bigClique/IPFP.ts
+++ src/inferences/bigClique/IPFP.ts
@@ -1,7 +1,15 @@
 import { ICliquePotentialItem } from '../../types'
 import { ISoftEvidence } from '../../types/ISoftEvidence'
 
-function getVariableMarginalDistribution (cliquePotential: ICliquePotentialItem[], variable: string): Record<string, number> {
+/**
+ *
+ * Extract the probability of the states of a variable independent of the rest of the clique potentials (Marginal distribution).
+ *
+ * @returns {Record<string, number>} Returns a map of states with their probabilities.
+ * @param cliquePotential The big clique in which the soft evidence is applied (Do not modify the original big clique potential, create a copy instead).
+ * @param variable The variable use for the marginal distribution.
+ */
+export function getVariableMarginalDistribution (cliquePotential: ICliquePotentialItem[], variable: string): Record<string, number> {
   const marginalDistribution: Record<string, number> = {}
   for (const entry of cliquePotential) {
     const variableState = entry.when[variable]
@@ -18,7 +26,13 @@ function getVariableMarginalDistribution (cliquePotential: ICliquePotentialItem[
  *
  * IPFP (Iterative Proportional Fitting Procedure) modifies the probability table to satisfy 'Soft Evidence'
  * It iteratively adjusts the table so that the marginal probabilities match the desired target distributions,
- * while preserving the original correlations between variables as much as possible .
+ * while preserving the original correlations between variables as much as possible.
+ *
+ * For more information about IPFP, please refer to the following paper:
+ * "Soft evidential update for probabilistic multiagent systems"
+ * by Marco Valtorta, Young-Gyun Kim, Jiri Vomlel (2000)
+ *
+ * DOI: https://www.sciencedirect.com/science/article/pii/S0888613X01000561
  *
  * @returns {ICliquePotentialItem[]} Returns the big clique potential after applying the soft evidence.
  * @param bigCliquePotential The big clique in which the soft evidence is applied (Do not modify the original big clique potential, create a copy instead).
@@ -26,10 +40,14 @@ function getVariableMarginalDistribution (cliquePotential: ICliquePotentialItem[
  * @param epsilon The convergence threshold. Default: 0.0001
  */
 export function IPFP (bigCliquePotential: ICliquePotentialItem[], softEvidence: ISoftEvidence, epsilon = 0.0001): ICliquePotentialItem[] {
-  const MAX_ITERATIONS_SAFETY_LIMIT = 100
+  const MAX_ITERATIONS_SAFETY_LIMIT = 100 // This number is an approximation of the maximum number of iterations that can be performed without taking an excessive amount of time.
 
   const newCliquePotential: ICliquePotentialItem[] = JSON.parse(JSON.stringify(bigCliquePotential))
-  const porentialsMaxDifference = (oldPotential: ICliquePotentialItem[], newPotential: ICliquePotentialItem[]): number => {
+
+  /**
+   * Calculates the maximum difference between two potentials.
+   */
+  const potentialsMaxDifference = (oldPotential: ICliquePotentialItem[], newPotential: ICliquePotentialItem[]): number => {
     let maxDiff = 0
     for (let i = 0; i < oldPotential.length; i++) {
       const diff = Math.abs(oldPotential[i].then - newPotential[i].then)
@@ -58,7 +76,7 @@ export function IPFP (bigCliquePotential: ICliquePotentialItem[], softEvidence: 
         newCliquePotential[i].then *= factorPerState[variableState]
       }
     }
-    maxDifference = porentialsMaxDifference(previousCliquePotential, newCliquePotential)
+    maxDifference = potentialsMaxDifference(previousCliquePotential, newCliquePotential)
   }
 
   return newCliquePotential
